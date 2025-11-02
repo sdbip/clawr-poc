5e8ae2e6c4b4d4acdc98280b9bde0dc1b8cd60c6
Johan Segerfeldt <johan.segerfeldt@me.com>
2025-11-02 18:10:30 +0100

    Flatten cases

--- Sources/Parser/Unresolved/UnresolvedExpression.swift
+++ Sources/Parser/Unresolved/UnresolvedExpression.swift
@@ -1,21 +1,21 @@
 import Lexer
 
-enum UnresolvedExpression {
-    case boolean(Located<Bool>)
-    case integer(Located<Int64>)
-    case real(Located<Double>)
-    case bitfield(Located<UInt64>)
-    case identifier(Located<String>)
+enum UnresolvedExpression: Equatable {
+    case boolean(Bool, location: FileLocation)
+    case integer(Int64, location: FileLocation)
+    case real(Double, location: FileLocation)
+    case bitfield(UInt64, location: FileLocation)
+    case identifier(String, location: FileLocation)
 }
 
 extension UnresolvedExpression {
     var location: FileLocation {
         switch self {
-        case .boolean((_, let l)),
-             .integer((_, let l)),
-             .bitfield((_, let l)),
-             .real((_, let l)),
-             .identifier((_, let l)):
+        case .boolean(_, let l),
+             .integer(_, let l),
+             .bitfield(_, let l),
+             .real(_, let l),
+             .identifier(_, let l):
                 return l
         }
     }
@@ -26,26 +26,26 @@ extension UnresolvedExpression {
 
         func expr() throws -> UnresolvedExpression {
             switch token.value {
-            case "true": return .boolean((true, location: token.location))
-            case "false": return .boolean((false, location: token.location))
+            case "true": return .boolean(true, location: token.location)
+            case "false": return .boolean(false, location: token.location)
             case let v where token.kind == .decimal:
                 if let i = Int64(v.replacing("_", with: "")) {
-                    return .integer((i, location: token.location))
+                    return .integer(i, location: token.location)
                 } else if let r = Double(v.replacing("_", with: "")) {
-                    return .real((r, location: token.location))
+                    return .real(r, location: token.location)
                 }
                 throw ParserError.invalidToken(token)
 
             case let v where token.kind == .binary:
                 if v.hasPrefix("0x"), let b = UInt64(v.dropFirst(2).replacing("_", with: ""), radix: 16) {
-                    return .bitfield((b, location: token.location))
+                    return .bitfield(b, location: token.location)
                 } else if v.hasPrefix("0b"), let b = UInt64(v.dropFirst(2).replacing("_", with: ""), radix: 2) {
-                    return .bitfield((b, location: token.location))
+                    return .bitfield(b, location: token.location)
                 }
                 throw ParserError.invalidToken(token)
 
             case let v where token.kind == .identifier:
-                return .identifier((v, location: token.location))
+                return .identifier(v, location: token.location)
 
             default:
                 throw ParserError.invalidToken(token)
@@ -56,11 +56,11 @@ extension UnresolvedExpression {
     func resolve(in scope: Scope) throws -> Expression {
 
         switch self {
-        case .boolean((let b, _)): return .boolean(b)
-        case .integer((let i, _)): return .integer(i)
-        case .real((let b, _)): return .real(b)
-        case .bitfield((let b, _)): return .bitfield(b)
-        case .identifier((let v, let location)):
+        case .boolean(let b, _): return .boolean(b)
+        case .integer(let i, _): return .integer(i)
+        case .real(let b, _): return .real(b)
+        case .bitfield(let b, _): return .bitfield(b)
+        case .identifier(let v, let location):
             guard let variable = scope.variable(forName: v) else { throw ParserError.unknownVariable(v,  location) }
             return .identifier(v, type: variable.type)
         }
--- Tests/ParserTests/UnresolvedExpressionTests.swift
+++ Tests/ParserTests/UnresolvedExpressionTests.swift
@@ -8,7 +8,7 @@ struct UnresolvedExpressionTests {
     func integer_with_grouping() async throws {
         let stream = TokenStream(source: "1_2_3")
         let expr = try UnresolvedExpression.parse(stream: stream)
-        guard case .integer((let value, _)) = expr else { Issue.record(); return }
+        guard case .integer(let value, _) = expr else { Issue.record(); return }
         #expect(value == 123)
     }
 
@@ -16,7 +16,7 @@ struct UnresolvedExpressionTests {
     func real_with_grouping() async throws {
         let stream = TokenStream(source: "1_2.3")
         let expr = try UnresolvedExpression.parse(stream: stream)
-        guard case .real((let value, _)) = expr else { Issue.record(); return }
+        guard case .real(let value, _) = expr else { Issue.record(); return }
         #expect(value == 12.3)
     }
 
@@ -24,7 +24,7 @@ struct UnresolvedExpressionTests {
     func hex_bitfield_with_grouping() async throws {
         let stream = TokenStream(source: "0x1_2_3")
         let expr = try UnresolvedExpression.parse(stream: stream)
-        guard case .bitfield((let value, _)) = expr else { Issue.record(); return }
+        guard case .bitfield(let value, _) = expr else { Issue.record(); return }
         #expect(value == 0x123)
     }
 
@@ -32,7 +32,7 @@ struct UnresolvedExpressionTests {
     func binary_bitfield_with_grouping() async throws {
         let stream = TokenStream(source: "0b1_1_0")
         let expr = try UnresolvedExpression.parse(stream: stream)
-        guard case .bitfield((let value, _)) = expr else { Issue.record(); return }
+        guard case .bitfield(let value, _) = expr else { Issue.record(); return }
         #expect(value == 0b110)
     }
 }
